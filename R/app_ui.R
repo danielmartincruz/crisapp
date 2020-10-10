e3be2d0195c170698b4326fb9a3e70bd0f31b5c5
Daniel Martin Cruz <danielmartincruz@MacBook-Pro-de-Daniel.local>
2020-10-10 20:53:57 +0200

    artist book

--- R/app_ui.R
+++ R/app_ui.R
@@ -14,7 +14,7 @@ app_ui <- function(time_preprocessing, commission) {
     # Define this page as a dashboard page to signal we're using the     dashboard page format
     shinydashboardPlus::dashboardPagePlus(
       
-      header = dashboardHeader(title = title, 
+      header = dashboardHeader(title = "Artist book", 
                         titleWidth = 600,
                         tags$li(class="dropdown",tags$a(href="https://www.instagram.com/cristinajimenez_artist/", icon("instagram"), "Instagram", target="_blank")),
                         tags$li(class="dropdown",tags$a(href="https://www.linkedin.com/in/abhinav-agrawal-pmp%C2%AE-itil%C2%AE-5720309/" ,icon("linkedin"), "My Profile", target="_blank")),
